6ba9b94aacaa58092144b9b1b2dc7c322856733b
Jonathan <jonathanbraum@gmail.com>
2022-03-04 03:20:16 -0300

    codigos var e conversoes

---
+++ conversoes.js
@@ -0,0 +1,17 @@
+// conversao implicita
+
+/* const numero = 456;
+const numeroString = '456';
+
+console.log(numero + numeroString)
+ */
+
+
+// coversao explicita
+const numero = 456;
+const numeroString = '456';
+
+console.log(numero + Number(numeroString))
+// Number ()
+// String()
+
